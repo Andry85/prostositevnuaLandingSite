f4969967bee0f18a94ce4f20497ca67e49f5cabb
HOM\Shap <shapovala@ukr.net>
2018-09-08 23:07:25 +0200

    added youtube

--- wp-content/themes/prostosaittheme/footer.php
+++ wp-content/themes/prostosaittheme/footer.php
@@ -2,7 +2,10 @@
     <footer class="footer">
         <ul class="social">
             <li class="social__item">
-                <a class="fb" target="_blank" href="https://www.facebook.com/prostositevn/" title=""></a>
+                <a class="fb" target="_blank" href="https://www.facebook.com/prostosite.vn.ua/?modal=admin_todo_tour" title=""></a>
+            </li>
+            <li class="social__item">
+                <a class="youtube" target="_blank" href="https://www.youtube.com/channel/UCA0VWdWrTvG2VUUwtK4aUNw" title=""></a>
             </li>
         </ul>
     </footer>
